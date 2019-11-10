f45c6541421acc27da474c708c8718a7dd44d300
Ben <ben@stolovitz.com>
2019-11-09 17:07:15 -0800

    try a control

--- src/js/Main.tsx
+++ src/js/Main.tsx
@@ -1,12 +1,48 @@
 import React from "react";
 
-export default class Main extends React.Component
+import Scheduler, { INote } from "./audio/Scheduler";
+import { generateMorseNotes } from "./audio/morse";
+
+function createAudioContext(): AudioContext {
+    return new (window.AudioContext || (window as any).webkitAudioContext)();
+}
+
+interface MainState {
+    currentLetters: string[]
+}
+
+export default class Main extends React.Component<{}, MainState>
 {
+    state = {
+        currentLetters: [
+            "m",
+            "k"
+        ]
+    };
+
+    private audioContext: AudioContext;
+    private scheduler: Scheduler;
+
+    constructor(props: {}) {
+        super(props);
+
+        this.audioContext = createAudioContext();
+        this.scheduler = new Scheduler(window, this.audioContext);
+        this.scheduler.start();
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            const notes = generateMorseNotes(this.audioContext, 'kmmkk');
+            this.scheduler.scheduleNotes(notes);
+        }, 300);
+    }
+
     render()
     {
         return (
             <section className="main">
-                <div className="letter">u</div>
+                <div className="letter">v</div>
                 <input type="text" />
             </section>
         );
---
+++ src/js/audio/morse.ts
@@ -0,0 +1,86 @@
+import { INote } from "./Scheduler";
+import { generateSineNote } from "./sine";
+
+function getMorseForCharacter(char: string) {
+    const MORSE_ALPHABET: { [key: string]: string } = {
+        "k": "-.-",
+        "m": "--"
+    };
+
+    if (char.length > 1) {
+        throw new Error(`Use only one char. (${char})`);
+    }
+
+    const morse = MORSE_ALPHABET[char];
+    if (!morse) {
+        throw new Error(`Don't know morse for ${char}`);
+    }
+
+    return morse;
+}
+
+// Our letter time should be ~20WPM.
+// Assume 5 letters/word, 4 points/letter, 50% dits
+// ((2 * DIT + 2 * DART) * 5 + INTER_POINT * 4) * 20 + INTER_WORD * 19 = 60s
+// DART = 3 * DIT
+// INTER_POINT = DIT
+// INTER_WORD = 3 * DART = 9 * DIT
+// ((2 * DIT + 6 * DIT) * 5 + DIT * 4) * 20 + (9 * DIT) * 19 = 60s
+// ((8 * DIT) * 5 + DIT * 4) * 20 + (9 * DIT) * 19 = 60s
+// (44 * DIT) * 20 + (171 * DIT) = 60s
+// 880 * DIT + 171 * DIT = 60s
+// 1050 * DIT = 60s
+// DIT = .057142857s
+
+const DIT_DURATION = 0.06;
+const DART_DURATION = 3 * DIT_DURATION;
+const INTER_POINT_DURATION = DIT_DURATION;
+const INTER_CHARACTER_DURATION = 3 * DART_DURATION;
+
+export function generateMorseNotes(context: AudioContext, message: string, options: {
+    frequencyInHertz: number,
+} = {
+    frequencyInHertz: 443,
+}): INote[] {
+    const chars = message.split("");
+    const notes = chars.reduce<INote[]>((currentMorseChars, char) => {
+        const isFirstCharacter = (currentMorseChars.length === 0);
+
+        const points = getMorseForCharacter(char).split("");
+        const notes = points.reduce<INote[]>((currentPoints, point, pointIndex) => {
+            const isFirstPointInCharacter = (currentPoints.length === 0);
+            let timeOffset = 0;
+            if (isFirstPointInCharacter) {
+                if (isFirstCharacter) {
+                    timeOffset = 0;
+                } else {
+                    const previousCharacterPoint = currentMorseChars[currentMorseChars.length - 1];
+                    timeOffset = previousCharacterPoint.timeFromNowInSeconds + INTER_CHARACTER_DURATION;
+                }
+            } else {
+                const previousPoint = currentPoints[currentPoints.length - 1];
+                timeOffset = previousPoint.timeFromNowInSeconds + INTER_POINT_DURATION;
+            }
+            
+            const duration = (point === "-")
+                ? DART_DURATION
+                : DIT_DURATION;
+            const note = generateSineNote({
+                context: context, 
+                duration: duration,
+                frequencyInHertz: options.frequencyInHertz, 
+                timeFromNowInSeconds: timeOffset
+            });
+            return [
+                ... currentPoints,
+                ... note
+            ];
+        }, []);
+
+        return [
+            ... currentMorseChars,
+            ... notes
+        ];
+    }, []);
+    return notes;
+}
