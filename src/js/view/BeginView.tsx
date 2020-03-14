d9e941d0cdb150d1002d7258b4db87d8a426e6d5
Ben <ben@stolovitz.com>
2020-03-14 15:20:15 -0400

    change cookie notice

--- src/js/view/BeginView.tsx
+++ src/js/view/BeginView.tsx
@@ -14,9 +14,10 @@ export default class BeginView extends React.Component<BeginViewProperties> {
             <section className="main">
                 <h1>Learn Morse!</h1>
                 <p>
-                    This app uses cookies to track your progress. By clicking
-                    OK, you consent to a cookie that tracks your lesson state
-                    and accuracy history.
+                    This app uses local storage to track your progress. By
+                    clicking OK, you consent to using local storage that tracks
+                    your lesson state and accuracy history. This information
+                    never leaves your computer.
                 </p>
                 <p>We use this to generate good lessons for you.</p>
                 <button onClick={this.handleBegin} className="startButton">
