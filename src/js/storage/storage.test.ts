df9f068cb000f6cdea34b89b784d5a9a1b3b4342
Ben <ben@stolovitz.com>
2020-03-14 12:50:05 -0400

    more tests

--- src/js/storage/storage.test.ts
+++ src/js/storage/storage.test.ts
@@ -1,21 +1,65 @@
 import IStorage from "./IStorage";
 import { getLearningState } from "./Storage";
+import { ILearningState } from "./LearningStateInterfaces";
 
 describe("getLearningState()", () => {
     it("should return default state when no state exists", () => {
-        const mockStorage: IStorage = {
-            hasStoredLearningState() {
-                return false;
-            },
-            readLearningState() {
-                throw new Error("Not implemented");
-            },
-            storeLearningState() {
-                throw new Error("Not implemented");
+        class MockStorage implements IStorage {
+            storeLearningState(learningState: ILearningState): void {
+                throw new Error("Method not implemented.");
             }
-        };
+            readLearningState(): ILearningState | null {
+                throw new Error("Method not implemented.");
+            }
+            hasStoredLearningState(): boolean {
+                return false;
+            }
+        }
 
-        const result = getLearningState(mockStorage);
+        const result = getLearningState(new MockStorage());
+        expect(result.isDefault).toBe(true);
+    });
+
+    it("should return state when it exists", () => {
+        const state: ILearningState = {
+            currentLesson: 5,
+            history: []
+        };
+        class MockStorage implements IStorage {
+            storeLearningState(learningState: ILearningState): void {
+                throw new Error("Method not implemented.");
+            }
+            readLearningState(): ILearningState | null {
+                return state;
+            }
+            hasStoredLearningState(): boolean {
+                return true;
+            }
+        }
+
+        const result = getLearningState(new MockStorage());
+        expect(result.isDefault).toBe(false);
+        expect(result.learningState).toBe(state);
+    });
+
+    it("should return default state when we are on lesson 1", () => {
+        const state: ILearningState = {
+            currentLesson: 1,
+            history: []
+        };
+        class MockStorage implements IStorage {
+            storeLearningState(learningState: ILearningState): void {
+                throw new Error("Method not implemented.");
+            }
+            readLearningState(): ILearningState | null {
+                return state;
+            }
+            hasStoredLearningState(): boolean {
+                return true;
+            }
+        }
+
+        const result = getLearningState(new MockStorage());
         expect(result.isDefault).toBe(true);
     });
 });
